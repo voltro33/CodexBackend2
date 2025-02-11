6b8c91d005e153a2ed5758a3047ffcb11077dd79
RafeM <138534376+voltro33@users.noreply.github.com>
2025-02-10 21:31:22 -0500

    Update todoServer.js

--- todoServer.js
+++ todoServer.js
@@ -177,7 +177,7 @@ const {idea} = req.body;
 
   try {
     const completion = await openai.chat.completions.create({
-      model: 'o3-mini', //changed jt from 'gpt-4o' to 'o3-mini'
+      model: 'gpt-3o-mini', //changed jt from 'gpt-4o' to 'o3-mini'
       messages: [
         {
           role: 'system',
